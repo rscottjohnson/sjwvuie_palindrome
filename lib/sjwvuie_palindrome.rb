3f71fa6cdee3deb1d2abf072e13034ad38a3b0dc
rscottjohnson <sjohnson.wvu@gmail.com>
2020-11-30 16:50:40 -0500

    8-5 refactoring

--- lib/sjwvuie_palindrome.rb
+++ lib/sjwvuie_palindrome.rb
@@ -1,6 +1,17 @@
 require "sjwvuie_palindrome/version"
 
-module SjwvuiePalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
+
 end
